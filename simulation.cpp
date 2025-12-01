0347bb3518d6b32f6912f80747a573bbecc63667
jetspeedy18 <brendan.moskalik@gmail.com>
2025-12-01 13:33:26 -0800

    working simulation and controller

--- simulation.cpp
+++ simulation.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <fstream>
 #ifdef _WIN32 
 	#include <windows.h>
 #endif
 #include <chrono>
+#include<vector>
 #include "system.cpp"
+#include "Controller.cpp"
 
-#include "matplotlibcpp.h"
+//#include "matplotlibcpp.h"
 
-namespace plt = matplotlibcpp;
+//namespace plt = matplotlibcpp;
 
 #define TICK_SPEED 60 //Hz to limit loop to
-#define DISTURBANCE_CHANCE 250 //what are the odds of getting a distrubance this cycle
+#define DISTURBANCE_CHANCE 500 //what are the odds of getting a distrubance this cycle
 
 #define NANOS_PER_LOOP 1000000000/TICK_SPEED
+#define DIRAC 10
+
+//using namespace std;
 
 int main(){
 	std::cout << "starting simulation" << std::endl;
@@ -21,13 +27,23 @@ int main(){
 	/// TODO
 	// create system
 	
-	System model = System();
+	System model(0,1,-2,-3,0,1,0);
+	
+	System model_unc(0,1,-2,-3,0,1,0);
+	
+	Controller cont;
 	
 	auto past = std::chrono::high_resolution_clock::now();
 	auto start = past;
+	double deltat = std::chrono::duration<double>( (past-start) ).count();
+	
 	std::vector<double> t, y;
 	
 	bool runing = true;
+	
+	std::ofstream out;
+	out.open("plots.csv");
+	
 	while (runing) {
 		// main simultation loop
 		
@@ -36,28 +52,41 @@ int main(){
 			
 			// Dirac function aplplied as input
 			
-			std::cout << "Dirac" << std::endl;
-			model.disturb();
+			model.setInput(DIRAC);
+			model_unc.setInput(DIRAC);
 			
-		} 
-				
+		} else if ((rand()%DISTURBANCE_CHANCE)==4) {
+			
+			// negative dirac funtion applied
+			model.setInput(-DIRAC);
+			model_unc.setInput(-DIRAC);
+		}
+			
+		double c = cont.control(model.gety(), deltat);
+		model.setInput(c);
+			
 			
 		// get pid values
 		// TODO// run model
-		model.update();
+		model.update(deltat);
+		model_unc.update(deltat);
 		//save values
-		double t_ = (past-start)/1000000000; //units [seconds]
+		double t_ = std::chrono::duration<double>( (past-start) ).count(); //units [seconds]
 		t.push_back(t_);
-		y.push_back(model.gety());
-		
-		//lets do some graphing - example of continous matplotlibcpp located at
-		//https://github.com/lava/matplotlib-cpp/blob/master/examples/animation.cpp
-		plt::clf();
-		plt::plot(t,y);
-		// todo standardaize limits and 
+		double y_ = model.gety();
+		double yd_ = model.getyd();
+		y.push_back(y_);
 		
 		
-
+		
+		double y__ = model_unc.gety();
+		double yd__ = model_unc.getyd();
+		
+		model.edita21(t_*0.5-9.0);
+		model_unc.edita21(t_*0.5-9.0);
+		model.edita22(t_*0.5-9.0);
+		model_unc.edita22(t_*0.5-9.0);
+		
 		
 		// key commands (WINDOWS IMPLEMENTED ONLY RN)
 		// other OS these inputs will not work
@@ -85,9 +114,15 @@ int main(){
 		
 		if (!didWait) std::cout << "WAIT UUUPPPP..." << std::endl;
 		
+		deltat = std::chrono::duration<double>( (now-past) ).count();;
 		past = now;
 		
+		//save the data to file
+		out << t_ << "," << y_ << ","  << y__ << "\n";
 	}
+	
+	//close file
+	out.close();
 	
 	
 	std::cout << "finished simulation" << std::endl;
--- system.cpp
+++ system.cpp
@@ -1,53 +1,78 @@
-
+#include<iostream>
 
 class System {
 private:
+
+	// INPUT
 	double u_;
-	double A;
-	double B;
 	
-	double x;
+	// A MATRIX
+	double a11, a12;
+	double a21, a22;
+		
+	// B MATRIX
+	double b1, b2;
 	
-	double f;
+	// CURRENT X STATES
+	double x1, x2;
+	
 
 public:
 	
-	System() {	System(1,1,0);	}
+	System() {	System(0,1,1,1,0,1,0);	}
 	
-	System(double a, double b, double x0){
-		A = a;
-		B = b;
-		x = x0;
+	System(double a11_, double a12_, double a21_, double a22_, double b1_, double b2_, double x0){
+		a11 = a11_;
+		a12 = a12_;
+		a21 = a21_;
+		a22 = a22_;
+		b1 = b1_;
+		b2 = b2_;
+		
+		x1 = x0;
+		x2 = 0;
+		
 		u_ = 0;
-		f = 0;
 	}
 	
 	void setInput(double u) {
-		u_ = u;
+		u_ += u;
 	}
 	
-	double getStates(double x) {
-		return A*x*B*u_;
-	}		
+	void edita21(double a) { a21 = a; }
+	void edita22(double a) { a22 = a; }
 	
-	void distrub() {
-		f = 10;
-		//to do random function or input?
+//	double getStateOneDot() {return a11*x1+a12*x2+b1*u_; }
+//	double getStateTwoDot() {return a21*x1+a22*x2+b2*u_; }		
+
+	
+	void update(double t) {
+		// update system
+		tickSimu(t);
+		// clear the inputs
+		u_ = 0;
 	}
 	
-	void update() {
-		tickSimul()
+	double gety() {
+		return x1;
 	}
 	
+	double getyd() {
+		return x2;
+	}
 	
 	// simulates step of system
-	void tickSimu() {
-		//x. = ax + bu_
-		double xd = getStates(x);
-		// solve the ode
-		double xnew = 0; //solution
+	void tickSimu(double dt) {
 		
-		
+		// just use euler steps - for what im doing this should be enough
+		double dx1 = a11 * x1 + a12 * x2 + b1*u_;
+		double dx2 = a21 * x1 + a22 * x2 + b2*u_;
+
+		// Forward Euler update
+		x1 += dt * dx1;
+		x2 += dt * dx2;
+
+	
 	}
 	
 };
