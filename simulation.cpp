0912c6dab6bf3f2d0cce1d31ee894923e7eee67e
jetspeedy18 <brendan.moskalik@gmail.com>
2025-12-04 19:51:23 -0800

    comments

--- simulation.cpp
+++ simulation.cpp
@@ -8,10 +8,8 @@
 #include "system.cpp"
 #include "Controller.cpp"
 
-//#include "matplotlibcpp.h"
 
-//namespace plt = matplotlibcpp;
-
+// lets define some compiler constants - these parameterize the compilation
 #define TICK_SPEED 60 //Hz to limit loop to
 #define DISTURBANCE_CHANCE 500 //what are the odds of getting a distrubance this cycle
 
@@ -24,30 +22,42 @@ int main(){
 	std::cout << "starting simulation" << std::endl;
 	
 	// load system parameters
-	/// TODO
 	// create system
 	
+	// controlled system with params
 	System model(0,1,-2,-3,0,1,0);
 	
+	// uncontrolled systemn with params for comparison
 	System model_unc(0,1,-2,-3,0,1,0);
 	
+	// define the controlleer
+	// this uses fuzzy logic autotuning within itself.
 	Controller cont;
 	
+	// keep track of time
 	auto past = std::chrono::high_resolution_clock::now();
 	auto start = past;
 	double deltat = std::chrono::duration<double>( (past-start) ).count();
 	
+	//keep track of data
 	std::vector<double> t, y;
 	
-	bool runing = true;
 	
+	// set up loop vars
+	bool runing = true;
 	std::ofstream out;
-	out.open("plots.csv");
+	out.open("plots.csv"); // dump output data to this file
 	
 	while (runing) {
 		// main simultation loop
+		// on windows `q` will exit the loop, otherwise you need to kill the program
+		// runs the simulation in real time with timesteps taken matching its update cycle
+		// this means that if we to try to have things change at a specific time, we can 
+		// easily convert that to number of loops
 		
 		
+		// disturbances are added randommly and at a random dirrection
+		// do this to both models
 		if ((rand()%DISTURBANCE_CHANCE)==3) {
 			
 			// Dirac function aplplied as input
@@ -61,40 +71,50 @@ int main(){
 			model.setInput(-DIRAC);
 			model_unc.setInput(-DIRAC);
 		}
-			
+		
+		
+		// for the controlled system we get the control input, and feed that into the system
+		// this is only for the controlled system
 		double c = cont.control(model.gety(), deltat);
 		model.setInput(c);
 			
 			
-		// get pid values
-		// TODO// run model
+		// update the models based on its inputs
+		// do this to both models
 		model.update(deltat);
 		model_unc.update(deltat);
-		//save values
+		
+		//save values - keep a bunhc in memory to be used or not later
 		double t_ = std::chrono::duration<double>( (past-start) ).count(); //units [seconds]
 		t.push_back(t_);
 		double y_ = model.gety();
 		double yd_ = model.getyd();
 		y.push_back(y_);
 		
-		
-		
+		// and for comparison
 		double y__ = model_unc.gety();
 		double yd__ = model_unc.getyd();
 		
+		
+		// next section is for chnaging model during runtime
+		// uncomment to have the model parameters be a function of runtime
+		/*
 		model.edita21(t_*0.5-9.0);
 		model_unc.edita21(t_*0.5-9.0);
+		
+		
 		model.edita22(t_*0.5-9.0);
 		model_unc.edita22(t_*0.5-9.0);
+		*/
 		
 		
 		// key commands (WINDOWS IMPLEMENTED ONLY RN)
-		// other OS these inputs will not work
+		// other OS these inputs will not work - will just skip this
 		#ifdef _WIN32
 			// hex reference for windws.h keys used below
 			// https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 			
-			// exit loop --> exits on q press
+			// EXIT loop --> exits on q press
 			SHORT exit = GetKeyState(0x51);
 			if (exit & 0x8000) {
 				runing = false;
@@ -112,8 +132,11 @@ int main(){
 			didWait = true;
 		}
 		
+		// lets just make sure our computer is running at real time
+		// this line should NOT be printed, otherwise loop parameters are too agressive
 		if (!didWait) std::cout << "WAIT UUUPPPP..." << std::endl;
 		
+		// save old time data
 		deltat = std::chrono::duration<double>( (now-past) ).count();;
 		past = now;
 		
--- system.cpp
+++ system.cpp
@@ -19,8 +19,11 @@ private:
 
 public:
 	
+	// create a system with some blank parameters
 	System() {	System(0,1,1,1,0,1,0);	}
 	
+	// cerate a system with specified parameters
+	// assumes a simple 2-state system, can easily be expanded to include more
 	System(double a11_, double a12_, double a21_, double a22_, double b1_, double b2_, double x0){
 		a11 = a11_;
 		a12 = a12_;
@@ -35,17 +38,19 @@ public:
 		u_ = 0;
 	}
 	
+	// added the input to apply in the next timestep
+	// this makes inputs function as dirac functions applied on the timesteps
 	void setInput(double u) {
 		u_ += u;
 	}
 	
+	
+	// change the system params on the fly
+	// since we assume x1 is pos and x2 is velocity we only need to update the bottom row of the a matrix
 	void edita21(double a) { a21 = a; }
 	void edita22(double a) { a22 = a; }
 	
-//	double getStateOneDot() {return a11*x1+a12*x2+b1*u_; }
-//	double getStateTwoDot() {return a21*x1+a22*x2+b2*u_; }		
-
-	
+	// called each timestep
 	void update(double t) {
 		// update system
 		tickSimu(t);
@@ -53,13 +58,11 @@ public:
 		u_ = 0;
 	}
 	
-	double gety() {
-		return x1;
-	}
+	// get position out
+	double gety() {	return x1; }
 	
-	double getyd() {
-		return x2;
-	}
+	// get veclocity out
+	double getyd() { return x2; }
 	
 	// simulates step of system
 	void tickSimu(double dt) {
